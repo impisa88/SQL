9ac5380073b19014f2ea543b4888094b8f2d2c3a
Joao Gilberto da Silva Hoffmann <JOAOSH@edu.unisinos.br>
2019-09-30 18:50:20 -0300

    TA atualizado

--- TA.sql
+++ TA.sql
@@ -32,16 +32,17 @@ CREATE TABLE "Desempenho" (
 );
 
 CREATE TABLE "Time" (
-	"Nome" TEXT
+	"Nome_time" TEXT,
+	PRIMARY KEY ("Nome")
 );
 
 CREATE TABLE "Partida"	(
 	"Data" date,
 	"Resultado" INTEGER,
 	id_partida INTEGER,
-	Nome TEXT,
+	Nome_time TEXT,
 	PRIMARY KEY ("id_partida"),
-	FOREIGN KEY ("Nome") REFERENCES "Time"("Nome")
+	FOREIGN KEY ("Nome_time") REFERENCES "Time"("Nome_time")
 );
 	
 create TABLE "Campeonato" (
@@ -49,5 +50,29 @@ create TABLE "Campeonato" (
 	id_camp INTEGER,
 	Ano INTEGER,
 	id_partida INTEGER,
+	PRIMARY KEY ("id_camp"),
 	FOREIGN KEY ("id_partida") REFERENCES "Partida"("id_partida")
 );
+
+create TABLE "Contrato" (
+	Num_contrato  INTEGER,
+	id_camp INTEGER,
+	id_jogador INTEGER,
+	Nome_time TEXT,
+	PRIMARY KEY ("Num_contrato"),
+	FOREIGN KEY ("id_camp") REFERENCES "Campeonato"("id_camp"),
+	FOREIGN KEY ("Nome_time") REFERENCES "Time"("Nome_time"),
+	FOREIGN KEY ("id_jogador") REFERENCES "Jogador"("id_jogador")
+);
+
+-- to do 
+
+CREATE VIEW "GolsPorTime" (Nome, Nome_time, G) as SELECT A.G as Gols from Desempenho A
+	
+	WHERE Nome = 1;
+
+CREATE VIEW "GolsPorTime" as
+	
+CREATE VIEW "GolsPorTime" as
+	
+CREATE VIEW "GolsPorTime" as
