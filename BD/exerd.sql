50c1acec2900a37f50dc3bf262838617b1686773
João Hoffmann <51866341+impisa88@users.noreply.github.com>
2019-10-28 16:31:16 -0300

    Update exerd.sql

--- BD/exerd.sql
+++ BD/exerd.sql
@@ -6,7 +6,7 @@ create trigger MonitoraBarco BEFORE insert ON Reservas
   for each row 
 
   begin
-	IF ((SELECT id_barco FROM Reservas WHERE devolucao IS NULL) AND new.id_barco = id_barco ) THEN
+	IF ((SELECT id_barco FROM Reservas WHERE devolucao IS NULL) AND new.id_barco = id_barco AND new.id_marin = id_marin) THEN
 		signal sqlstate '45000' set MESSAGE_TEXT = 'Este barco está reservado';
 	END IF;
   END; $$
